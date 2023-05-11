1b1b70e6626e5589cc6ca75c6db59bab3de61840
Sofia Lazarte <sofi.lazarte.sl@gmail.com>
2023-05-11 16:46:03 -0300

    primeros 4 puntos

---
+++ archivo.js
@@ -0,0 +1,18 @@
+//punto uno
+
+alert ("un mensaje")
+
+//punto dos
+
+document.write ("Hello Word.  ")
+
+//punto tres
+
+document.write (" El resultado de sumar 3+5 es igual a " +  (3+5))
+
+//punto cuatro 
+
+let nombre= prompt ("Escribe tu nombre")
+
+document.write ("  Hola  "  +  nombre)
+
