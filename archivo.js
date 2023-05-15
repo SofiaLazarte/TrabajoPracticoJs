9a886e6f36c5548c008feb74b9ef8d91a383030f
Sofia Lazarte <sofi.lazarte.sl@gmail.com>
2023-05-15 11:36:48 -0300

    punto seis

--- archivo.js
+++ archivo.js
@@ -1,23 +1,38 @@
 //punto uno
 
-alert ("un mensaje")
+alert("un mensaje");
 
 //punto dos
 
-document.write ("Hello Word.  ")
+document.write("Hello Word.  ");
 
 //punto tres
 
-document.write (" El resultado de sumar 3+5 es igual a " +  (3+5))
+document.write(" El resultado de sumar 3+5 es igual a " +  (3+5));
 
 //punto cuatro 
 
-let nombre= prompt ("Escribe tu nombre")
+let nombre = prompt ("Escribe tu nombre");
 
-document.write ("  Hola  "  +  nombre)
+document.write("  Hola  "  +  nombre);
 
 //punto cinco 
 
-let numeroUno= prompt ("Escribe un numero")
-let numeroDos= prompt ("Escribe otro numero")
-document.write (" el resultado de numeroUno y numeroDos es " + (parseInt(numeroUno) + parseInt(numeroDos))) 
+let numeroUno = prompt ("Escribe un numero");
+let numeroDos = prompt ("Escribe otro numero");
+document.write(" el resultado de numeroUno y numeroDos es " + (parseInt(numeroUno) + parseInt(numeroDos))) ;
+
+//punto seis
+
+let a = prompt("Escribe primer numero");
+let b = prompt("Escribe segundo numero");
+
+if ((parseInt(a)) > (parseInt(b))) 
+{
+    document.write("el numero mayor es el " + a);
+}
+else 
+{
+    document.write("el numero mayor es el " + b);
+}
+
